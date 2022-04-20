00b2b3a952817bfaa8fcd87df3c3c1fdd549bee9
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-04-20 16:14:41 -0400

    complete basic back

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,18 @@
 class CarsController < ApplicationController
+  def index
+    cars = Car.all
+    render json: cars.as_json
+  end
+
+  def create
+    car = Car.new(
+      make: params[:make]
+      model: params[:model]
+      color: params[:color]
+      msrp: params[:msrp]
+      img_url: params[:img_url]
+    )
+
+    car.save
+  end
 end
