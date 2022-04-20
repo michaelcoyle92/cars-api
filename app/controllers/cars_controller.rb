799db56f5ac27eab83e73644b97ec165a82f8ca0
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-04-20 16:26:09 -0400

    gems added

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,16 +6,16 @@ class CarsController < ApplicationController
 
   def show
     car = Car.find_by(id: params[:id])
-    render car
+    render json: car.as_json
   end
 
   def create
     car = Car.new(
-      make: params[:make]
-      model: params[:model]
-      color: params[:color]
-      msrp: params[:msrp]
-      img_url: params[:img_url]
+      make: params[:make],
+      model: params[:model],
+      color: params[:color],
+      msrp: params[:msrp],
+      img_url: params[:img_url],
     )
 
     car.save
